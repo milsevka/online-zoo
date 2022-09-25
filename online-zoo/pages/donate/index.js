88cd7b1e5ef251845b835a7eb7d810e03a86059e
Milsevka <lusindami99@gmail.com>
2022-09-25 15:28:01 +0300

    feat: add margin in cards

---
+++ online-zoo/pages/donate/index.js
@@ -0,0 +1,19 @@
+
+// const dot = document.querySelectorAll(".slider_dot")
+// const number = document.querySelectorAll(".slider_number")
+
+// dot.forEach(c => {
+//     c.addEventListener('click', () => {
+//       dot.forEach(s => s.classList.remove('meow'));
+//       dot.forEach(s => s.classList.remove('meow_back'));
+//       c.classList.add('meow');
+//       c.classList.add('meow_back');
+//     })
+//   })
+
+//   number.forEach (z => {
+//     z.addEventListener('click', () => {
+//         number.forEach(t => t.classList.remove('number_back'));
+//         z.classList.add('number_back')
+//     })
+//    })
