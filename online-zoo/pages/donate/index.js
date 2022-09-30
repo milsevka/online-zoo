b3acdf1edfd90fd2008ceab82198a3c5f363d91a
Milsevka <lusindami99@gmail.com>
2022-09-30 20:04:42 +0300

    feat: made all options in donate and add burger

--- online-zoo/pages/donate/index.js
+++ online-zoo/pages/donate/index.js
@@ -1,4 +1,4 @@
-let input = document.querySelector('.form-control-btn');
+let input = document.querySelector('.form-control-footer');
 input.addEventListener('input', onInput);
 const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
 function onInput() {
@@ -10,3 +10,28 @@ function onInput() {
 }
 function isEmailValid(value) {
 return EMAIL_REGEXP.test(value)};
+
+const menu = document.querySelector(".burger_block");
+const menuClose = document.querySelector(".close_burger");
+const menuItem = document.querySelector(".header_list-burger");
+const burgerItem = document.querySelector(".header_burger");
+
+burgerItem.addEventListener("click", () => {
+  menu.classList.toggle("burger_block_active");
+  burgerItem.classList.add('inactive')
+  // wrapper.classList.add("wrapper_mob_click_active");
+});
+menuClose.addEventListener("click", () => {
+  menu.classList.remove("burger_block_active");
+  burgerItem.classList.remove('inactive')
+  // wrapper.classList.remove("wrapper_mob_click_active");
+});
+menuItem.addEventListener("click", () => {
+  menu.classList.remove("burger_block_active");
+  burgerItem.classList.remove('inactive')
+  // wrapper.classList.remove("wrapper_mob_click_active");
+});
+// wrapperClick.addEventListener("click", () => {
+//   menu.classList.remove("burger_block_active");
+//   wrapper.classList.remove("wrapper_mob_click_active");
+// });
